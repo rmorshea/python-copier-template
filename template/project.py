3862d261ca644e614bb06756b9ebeca4e9d298b0
Ryan Morshead <ryan.morshead@gmail.com>
2024-11-24 15:38:04 -0700

    fix project

--- template/project.py
+++ template/project.py
@@ -150,7 +150,7 @@ else:
         kwargs.setdefault("check", True)
         click.echo(click.style(" ".join(cmd), bold=True))
         try:
-            return subprocess.run(*args, **kwargs)
+            return subprocess.run(cmd, *args, **kwargs)
         except subprocess.CalledProcessError as e:
             raise click.ClickException(e) from None
         except FileNotFoundError as e:
